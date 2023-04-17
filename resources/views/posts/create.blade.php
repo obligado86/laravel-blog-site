2f55cd9abfa07d5b97121af46e44ff3a92ad517b
@obligado86 <obligado86@yahoo.com>
2023-04-17 20:30:35 +0800

    add laravel-blog

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,19 @@
+@extends('layout.app')
+
+
+@section('content')
+	<form method="POST" action="/post">
+		@csrf
+		<div class="form-group">
+			<label for="title">Title:</label>
+			<input type="text" name="title" id="title" class="form-control">
+		</div>
+		<div class="form-group">
+			<label for="content">Content:</label>
+			<textarea class="form-control" id="content" name="content" rows="3"></textarea>
+		</div>
+		<div class="mt-2">
+			<button type="submit" class="btn btn-primary">Create Post</button>
+		</div>
+	</form>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\PostController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// define a route wherein a view to create a post will be returned to the user
+Route::get('/posts/create', [PostController::class, 'create']);
